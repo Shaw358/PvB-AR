8d1a8d40023f2ab002d4bbf3c0b6558d2c8ac265
CMDR <26555@ma-web.nl>
2022-03-09 16:54:54 +0100

    Adds initial script for icon loading

--- PvB/Assets/IconLoader.cs
+++ PvB/Assets/IconLoader.cs
@@ -10,11 +10,25 @@ public class IconLoader : MonoBehaviour
 
     public void UpdateIcons(ChapterType type)
     {
-        icons.Add(Instantiate(iconImages[(int)type], gameObject.transform));
+        /*for (int i = 0; i < length; i++)
+        {
+            icons.Add(Instantiate(iconImages[(int)type], gameObject.transform).GetComponent<RectTransform>());
+        }*/
     }
 
     private float GetLocationForIcon(int index, int totalAmountOfIconsInScrollBar)
     {
+        
+
         return 0;
+    }
+
+    private void ClearIcons()
+    {
+        foreach (RectTransform icon in icons)
+        {
+            icons.Remove(icon);
+            Destroy(icon.gameObject);
+        }
     }
 }
